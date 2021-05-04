552d1d85a4c0103029d553ceebb97f3bf8e65646
Renan0000 <75097629+Renan0000@users.noreply.github.com>
2021-05-04 20:34:37 -0300

    Update index.js
    adicionei o modo pra mostrar a foto

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,6 @@
 let meuapp = {
+    contatoSelecionado: null,
+
     inicializar: function(){
         console.log("inicializar: function(){...");
         document.addEventListener('deviceready',meuapp.onMyDeviceReady, false);
@@ -10,11 +12,29 @@ onMyDeviceReady: function(){
     document.getElementById("btnSelecionaContato").addEventListener('click',meuapp.selecionarContato);
 },
 
+retornoAoPrimeiroPlano:function(onresumeEvent){
+    console.log("#### -> retornoAoPrimeiroPlano:function(){...");
+},
+
 selecionarContato : function(){
     console.log("selecionarContato : function(){...");
     navigator.contacts.pickContact(function(c) {
         console.log("####=> Contacto Selecionado");
         console.log(c);
+        meuapp.contatoSelecionado = c;
+        console.log (c.displayName);
+        let spanElement = document.getElementById("nomeDoContato").innerHTML = c.displayName;
+        console.log(spanElement);
+
+        console.log(c.photos);
+        let imgElement = document.getElementById("imgDoContato");
+        console.log("TAG Imagem...");
+        console.log(imgDoContato);
+        console.log("Value do obj Imagem do array photos");
+        console.log(c.photos[0].value);
+
+        imgElement.src = c.photos[0].value;
+
     },
     function (err){
         console.log("####=> Contato não selecionado");
